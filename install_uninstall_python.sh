7c4a49726f2e549b5b1363c7b512a4f744faf9b2
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-08 13:08:13 +0800

    Update install_uninstall_python.sh

--- install_uninstall_python.sh
+++ install_uninstall_python.sh
@@ -1,10 +1,52 @@
 #!/bin/bash
 
-# 安装所需的开发包和Python运行时依赖项
+# 安装Python依赖项
 function install_dependencies() {
-    echo "正在安装所需的开发包和Python运行时依赖项..."
-    # 根据不同的发行版和包管理器添加安装依赖的命令
-    echo "依赖项安装完成。"
+    if [ "$LINUX_DISTRIBUTION" == "centos" ]; then
+        sudo yum install -y gcc make openssl-devel bzip2-devel libffi-devel zlib-devel
+    elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ] || [ "$LINUX_DISTRIBUTION" == "debian" ]; then
+        sudo apt-get update
+        sudo apt-get install -y build-essential libssl-dev libffi-dev zlib1g-dev
+    else
+        echo "不支持的发行版。"
+        exit 1
+    fi
+}
+
+# 安装Python
+function install_python() {
+    if command -v python3 &>/dev/null; then
+        echo "Python已安装。"
+    else
+        if [ "$LINUX_DISTRIBUTION" == "centos" ]; then
+            sudo yum install -y python3
+        elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ] || [ "$LINUX_DISTRIBUTION" == "debian" ]; then
+            sudo apt-get install -y python3
+        fi
+    fi
+}
+
+# 卸载Python
+function uninstall_python() {
+    if command -v python3 &>/dev/null; then
+        if [ "$LINUX_DISTRIBUTION" == "centos" ]; then
+            sudo yum remove -y python3
+        elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ] || [ "$LINUX_DISTRIBUTION" == "debian" ]; then
+            sudo apt-get remove -y python3
+        fi
+    else
+        echo "未找到已安装的Python版本。"
+    fi
+}
+
+# 检查Python版本
+function check_python_version() {
+    if command -v python3 &>/dev/null; then
+        echo "当前系统已安装的Python版本："
+        python3 --version
+    else
+        echo "未找到已安装的Python版本。"
+    fi
 }
 
 # 获取用户的Linux发行版
@@ -22,91 +64,35 @@ function get_linux_distribution() {
     fi
 }
 
-# 获取包管理器
-function get_package_manager() {
-    if command -v apt-get &>/dev/null; then
-        PACKAGE_MANAGER="apt-get"
-    elif command -v yum &>/dev/null; then
-        PACKAGE_MANAGER="yum"
-    elif command -v dnf &>/dev/null; then
-        PACKAGE_MANAGER="dnf"
-    else
-        PACKAGE_MANAGER="unknown"
-    fi
-}
+echo "欢迎使用Python交互式安装与管理脚本！"
 
-# 安装Python依赖项
-function install_python_dependencies() {
-    if [ "$LINUX_DISTRIBUTION" == "debian" ]; then
-        sudo $PACKAGE_MANAGER install -y build-essential libssl-dev libffi-dev zlib1g-dev
-    elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ]; then
-        sudo $PACKAGE_MANAGER install -y build-essential libssl-dev libffi-dev zlib1g-dev
-    elif [ "$LINUX_DISTRIBUTION" == "centos" ]; then
-        sudo $PACKAGE_MANAGER install -y gcc make openssl-devel bzip2-devel libffi-devel zlib-devel
-    else
-        echo "无法确定发行版或不支持的发行版。"
-        exit 1
-    fi
-}
+get_linux_distribution
 
-# 安装Python
-function install_python() {
-    # 根据用户的发行版和包管理器来安装Python
-    # ...
-}
+while true; do
+    echo "请选择以下选项："
+    echo "1. 安装Python"
+    echo "2. 卸载Python"
+    echo "3. 查看Python版本"
+    echo "4. 退出"
+    read -p "请输入选项编号: " choice
 
-# 卸载Python
-function uninstall_python() {
-    # 根据用户的发行版和包管理器来卸载Python
-    # ...
-}
-
-# 检查Python版本
-function check_python_version() {
-    if command -v python3 &>/dev/null; then
-        echo "当前系统已安装的Python版本："
-        python3 --version
-    else
-        echo "未找到已安装的Python版本。"
-    fi
-}
-
-# 主函数
-function main() {
-    echo "欢迎使用Python交互式安装与管理脚本！"
-
-    get_linux_distribution
-    get_package_manager
-
-    while true; do
-        echo "请选择以下选项："
-        echo "1. 安装Python"
-        echo "2. 卸载Python"
-        echo "3. 查看Python版本"
-        echo "4. 退出"
-        read -p "请输入选项编号: " choice
-
-        case "$choice" in
-            1)
-                install_dependencies
-                install_python_dependencies
-                install_python
-                ;;
-            2)
-                uninstall_python
-                ;;
-            3)
-                check_python_version
-                ;;
-            4)
-                echo "已退出脚本。"
-                break
-                ;;
-            *)
-                echo "无效的选项。"
-                ;;
-        esac
-    done
-}
-
-main
+    case "$choice" in
+        1)
+            install_dependencies
+            install_python
+            ;;
+        2)
+            uninstall_python
+            ;;
+        3)
+            check_python_version
+            ;;
+        4)
+            echo "已退出脚本。"
+            break
+            ;;
+        *)
+            echo "无效的选项。"
+            ;;
+    esac
+done
