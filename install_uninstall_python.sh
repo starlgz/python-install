2b9da07f843c692c734e4fa46923ec02158b0a28
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-10 11:24:13 +0800

    Update install_uninstall_python.sh

--- install_uninstall_python.sh
+++ install_uninstall_python.sh
@@ -1,98 +1,79 @@
-#!/bin/bash
+import tkinter as tk
+import subprocess
 
-# 安装Python依赖项
-function install_dependencies() {
-    if [ "$LINUX_DISTRIBUTION" == "centos" ]; then
-        sudo yum install -y gcc make openssl-devel bzip2-devel libffi-devel zlib-devel
-    elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ] || [ "$LINUX_DISTRIBUTION" == "debian" ]; then
-        sudo apt-get update
-        sudo apt-get install -y build-essential libssl-dev libffi-dev zlib1g-dev
-    else
-        echo "不支持的发行版。"
-        exit 1
-    fi
-}
+# 创建主窗口
+root = tk.Tk()
+root.title("Python功能菜单")
 
-# 安装Python
-function install_python() {
-    if command -v python3 &>/dev/null; then
-        echo "Python已安装。"
-    else
-        if [ "$LINUX_DISTRIBUTION" == "centos" ]; then
-            sudo yum install -y python3
-        elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ] || [ "$LINUX_DISTRIBUTION" == "debian" ]; then
-            sudo apt-get install -y python3
-        fi
-    fi
-}
+# 创建菜单栏
+menu_bar = tk.Menu(root)
 
-# 卸载Python
-function uninstall_python() {
-    if command -v python3 &>/dev/null; then
-        if [ "$LINUX_DISTRIBUTION" == "centos" ]; then
-            sudo yum remove -y python3
-        elif [ "$LINUX_DISTRIBUTION" == "ubuntu" ] || [ "$LINUX_DISTRIBUTION" == "debian" ]; then
-            sudo apt-get remove -y python3
-        fi
-    else
-        echo "未找到已安装的Python版本。"
-    fi
-}
+# 创建Python菜单
+python_menu = tk.Menu(menu_bar, tearoff=0)
 
-# 检查Python版本
-function check_python_version() {
-    if command -v python3 &>/dev/null; then
-        echo "当前系统已安装的Python版本："
-        python3 --version
-    else
-        echo "未找到已安装的Python版本。"
-    fi
-}
+# 创建安装Python菜单
+def install_python():
+    version = input("请输入要安装的Python版本：")
+    subprocess.call(["sudo", "apt-get", "install", "python" + version])
 
-# 获取用户的Linux发行版
-function get_linux_distribution() {
-    if [ -f /etc/os-release ]; then
-        source /etc/os-release
-        LINUX_DISTRIBUTION=$ID
-    elif [ -f /etc/lsb-release ]; then
-        source /etc/lsb-release
-        LINUX_DISTRIBUTION=$DISTRIB_ID
-    elif [ -f /etc/debian_version ]; then
-        LINUX_DISTRIBUTION="debian"
-    else
-        LINUX_DISTRIBUTION="unknown"
-    fi
-}
+install_menu = tk.Menu(python_menu, tearoff=0)
+install_menu.add_command(label="安装Python", command=install_python)
+python_menu.add_cascade(label="安装", menu=install_menu)
 
-echo "欢迎使用Python交互式安装与管理脚本！"
+# 创建卸载Python菜单
+def uninstall_python():
+    version = input("请输入要卸载的Python版本：")
+    subprocess.call(["sudo", "apt-get", "remove", "python" + version])
 
-get_linux_distribution
+uninstall_menu = tk.Menu(python_menu, tearoff=0)
+uninstall_menu.add_command(label="卸载Python", command=uninstall_python)
+python_menu.add_cascade(label="卸载", menu=uninstall_menu)
 
-while true; do
-    echo "请选择以下选项："
-    echo "1. 安装Python"
-    echo "2. 卸载Python"
-    echo "3. 查看Python版本"
-    echo "4. 退出"
-    read -p "请输入选项编号: " choice
+# 创建列出所有Python版本菜单
+def list_python_versions():
+    output = subprocess.check_output(["apt-cache", "search", "^python$"]).decode("utf-8")
+    versions = [line.split()[0] for line in output.split("\n") if line.startswith("python")]
+    print("所有Python版本：")
+    for version in versions:
+        print(version)
 
-    case "$choice" in
-        1)
-            install_dependencies
-            install_python
-            ;;
-        2)
-            uninstall_python
-            ;;
-        3)
-            check_python_version
-            ;;
-        4)
-            echo "已退出脚本。"
+list_menu = tk.Menu(python_menu, tearoff=0)
+list_menu.add_command(label="列出所有Python版本", command=list_python_versions)
+python_menu.add_cascade(label="列出版本", menu=list_menu)
+
+# 添加Python菜单到菜单栏
+menu_bar.add_cascade(label="Python", menu=python_menu)
+
+# 创建帮助菜单
+help_menu = tk.Menu(menu_bar, tearoff=0)
+help_menu.add_command(label="关于")
+menu_bar.add_cascade(label="帮助", menu=help_menu)
+
+# 显示菜单栏
+root.config(menu=menu_bar)
+
+# 定义交互式命令
+def interactive():
+    print("欢迎使用Python交互式命令行！")
+    while True:
+        command = input(">>> ")
+        if command == "exit":
             break
-            ;;
-        *)
-            echo "无效的选项。"
-            ;;
-    esac
-done
+        try:
+            exec(command)
+        except Exception as e:
+            print(e)
+
+# 创建交互式菜单
+interactive_menu = tk.Menu(menu_bar, tearoff=0)
+interactive_menu.add_command(label="进入交互式命令行", command=interactive)
+menu_bar.add_cascade(label="交互式", menu=interactive_menu)
+
+# 显示菜单栏
+root.config(menu=menu_bar)
+
+# 进入消息循环
+while True:
+    root.update()
+    root.update_idletasks()
+    input("按下回车键继续...")
